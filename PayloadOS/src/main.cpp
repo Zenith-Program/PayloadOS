ad759303cef91cff5a48d350598ca77f489e98eb
Nate-4-4 <153030151+Nate-4-4@users.noreply.github.com>
2025-03-01 16:54:54 -0500

    W/O Shitty Log

--- PayloadOS/src/main.cpp
+++ PayloadOS/src/main.cpp
@@ -2,21 +2,12 @@
 #include "PayloadOSConsoleIO.h"
 #include "PayloadOSConsoleInterpreter.h"
 #include "PayloadOSPeripheralSelector.h"
-#include "PayloadOSSDLog.h"
 
 using namespace PayloadOS;
 char buffer[64] = "original";
 
 void setup() {
   PayloadOS::SerialIO::get()->init();
-  if(Peripherals::Log::get()->initSD()){
-    Serial.println("SD Init Error");
-  }
-  else{
-    Serial.println("Init");
-  }
-  Peripherals::Log::get()->setDestination(Peripherals::LogDestinations::SDCard);
-  Peripherals::Log::get()->setFileName("TestLog.txt");
 }
 
 void loop() {
@@ -31,9 +22,6 @@ void loop() {
   delay(1000);
   float_t altitude = PayloadOS::Peripherals::PeripheralSelector::get()->getPayloadAltimeter()->getAltitude_m();
   Serial.println(buffer);
-  if(Serial.available()){
-    if(Serial.parseInt() == 1) Peripherals::Log::get()->getLine(buffer, 64);
-  }
  
 }
 
